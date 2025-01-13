3e0a264a6ded4e0be57195c549f144e937f7a95d
Alkis <alkisax@gmail.com>
2025-01-13 10:04:06 +0200

    13.1 ok

--- Exercise131.java
+++ Exercise131.java
@@ -1,7 +1,28 @@
 package gr.aueb.cf.ch13.exercises;
 
+import java.util.Random;
+
 public class Exercise131 {
     public static void main(String[] args) {
+        PointXYZ point = new PointXYZ(3,4,0);
 
+        Random random = new Random();
+        // Generate random x, y, and z values in the range [-10, 10]
+        int x = random.nextInt(21) - 10;
+        int y = random.nextInt(21) - 10;
+        int z = random.nextInt(21) - 10;
+
+        PointXYZ randoPoint = new PointXYZ(x, y, z);
+
+        tester(point);
+        tester(randoPoint);
+
+    }
+
+    public static void tester(PointXYZ point) {
+        System.out.println("XY Distance: " + point.getXYDistance());  // Expected output: 5
+        System.out.println("YZ Distance: " + point.getYZDistance());  // Expected output: 4
+        System.out.println("XZ Distance: " + point.getXZDistance());  // Expected output: 3
+        System.out.println("XYZ Distance: " + point.getXyZDistance()); // Expected output: 5
     }
 }
---
+++ PointXYZ.java
@@ -0,0 +1,66 @@
+package gr.aueb.cf.ch13.exercises;
+
+public class PointXYZ {
+    private int x;
+    private int y;
+    private int z;
+
+    public PointXYZ() {
+    }
+
+    public PointXYZ(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public int getXYDistance() {
+        return (int) Math.sqrt(Math.pow(this.getX(), 2) + Math.pow(this.getY(), 2));
+    }
+
+    public int getYZDistance() {
+        return (int) Math.sqrt(Math.pow(this.getY(), 2) + Math.pow(this.getZ(), 2));
+    }
+    public int getXZDistance() {
+        return (int) Math.sqrt(Math.pow(this.getX(), 2) + Math.pow(this.getZ(), 2));
+    }
+    public int getXyZDistance() {
+        return (int) Math.sqrt(
+                Math.pow(this.getX(), 2) +
+                Math.pow(this.getY(), 2) +
+                Math.pow(this.getZ(), 2)
+        );
+    }
+
+    public String convertToString() {
+        return "PointXYZ{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+}
